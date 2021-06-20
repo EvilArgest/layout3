cceb63ec2c246a41f78abbd1ce1bbd423719f4d2
EvilArgest <argest7997@gmail.com>
2021-06-20 18:20:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        speed: 1000,
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        fade: true,
+        draggable: false,
+        pauseOnFocus: false,
+        pauseOnDotsHover: false
+    });
+});
